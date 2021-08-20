4e9ec5b90d4ee54b41ba2a106cd67c5640a58ef6
Shamal Williams <shamal@Shamals-MacBook-Pro.local>
2021-08-20 17:35:49 -0400

    Problem solved: function and variable names were mixed up. Short term memory loss.

--- to-do-list/src/components/input.jsx
+++ to-do-list/src/components/input.jsx
@@ -4,14 +4,14 @@ const Input = (props)=>{
     const [inputText, inputTextSet] = useState(''); 
     const eventHandleChanges = (event)=>{
         const newInput = event.target.value;
-        setInputText(newInput);
+        inputTextSet(newInput);
     }
     return(
         <div className="form">
-            <input type = "text" onChange={handleChange} value={inputText} />
+            <input type = "text" onChange={eventHandleChanges} value={inputText} />
             <button onClick={() =>{
                 props.onAdd(inputText);
-                setTextSet("");
+                inputTextSet("");
             }}>
                 <span>Add</span>
             </button>
