c458a03eaca6fd2a4c9bdb4b1491ded7f82618c9
Shamal Williams <shamal@Shamals-MacBook-Pro.local>
2021-08-20 16:46:33 -0400

    Just added hooks

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Input from "./components/input"; 
+import { Input } from "./components/input"; 
 
 function App() {
+  const [theItems, setTheItems] = useState([]); 
   return (
     <div className="container">
       <div className="header">
