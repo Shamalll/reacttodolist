a7e00c36198ffce22a4a3f01961f9f5ccbcc35d6
Shamal Williams <shamal@Shamals-MacBook-Pro.local>
2021-08-20 16:41:12 -0400

    Added CSS and components so as to give it a more apprachable look.

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Input from "./components/input"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <h1>To do list</h1>
+      </div>
+      <br></br>
+      
+      <input />
     </div>
   );
 }
---
+++ to-do-list/src/components/input.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Input = ()=>{
+    return(
+        <div className="form">
+            <input type = "text" />
+            <button>
+                <span>Add</span>
+            </button>
+
+        </div>
+    )
+}
+
+export default Input; 
